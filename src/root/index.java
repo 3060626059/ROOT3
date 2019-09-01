05264695b4a2ad22db60b676e5d643d2d3045ca7
lenovo <lenovo@169.254.207.119>
2019-09-01 17:45:11 +0800

    可以发布的版本 2019年9月1日 17:45:08

--- src/root/index.java
+++ src/root/index.java
@@ -10,18 +10,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import root.tool.dowloadS;
 import root.tool.xtool;
 
 /**
- * Servlet implementation class index
- * 问题 不知道如何解决 / 路径下没办法读取 js ,css 文件
- * 但是 tomcat 404 却能加载 js , css 文件
+ * Servlet implementation class index 问题 不知道如何解决 / 路径下没办法读取 js ,css 文件 但是 tomcat
+ * 404 却能加载 js , css 文件
  */
-@WebServlet(name = "index",urlPatterns = {"/","/in","/index"})
+@WebServlet(name = "index", urlPatterns = { "/", "/in", "/index" })
 // 访问路径设置成 / 则无法访问静态资源
 public class index extends HttpServlet {
-
+	
 	private static final long serialVersionUID = 1L;
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -40,35 +38,43 @@ public class index extends HttpServlet {
 			response.setCharacterEncoding("utf-8");
 			response.setContentType("text/html;charset=utf-8");
 			
-			byte[] xbyte = xtool.x读取文本(new File(
-			                "C:\\Users\\lenovo\\Desktop\\AppProjects\\项目 05-28_搬移\\ROOT3\\WebRoot\\NewFile.html"));
-			response.addHeader("Content-Length", xbyte.length + "");
-			response.getOutputStream().write(xbyte);
-			response.flushBuffer();
+			// byte[] xbyte = xtool.x读取文本(new File(
+			// "C:\\Users\\lenovo\\Desktop\\AppProjects\\项目
+			// 05-28_搬移\\ROOT3\\WebRoot\\NewFile.html"));
+			// 使用发布后项目内的资源文件
+			// 而不是开发时的指定路径
+			
+			File x_HTML = new File(getServletContext().getRealPath(""), "NewFile.html");
+			// File x_HTML1 = new File("C:\\Users\\lenovo\\Desktop\\AppProjects\\项目 05-28_搬移\\ROOT3\\WebRoot\\", "NewFile.html");
+			
+			try {
+				xtool.x_HTML_to_Byte_to_Response(x_HTML, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
 		} else {
 			switch (xParameter.nextElement()) {
 				
 				case "x0":
-					// 视频请求
-					File x_文件 = new File("D:\\视频\\超兽武装\\超兽武装 - RevEvolution 35.mp4");
+					
+					// 异步请求 返回所有servlet的入口信息
+					StringBuilder x_info = xtool.x_servlet_urlPatterns_扫描_to_HTML(request);
 					try {
-						dowloadS.x_传输流(request, response, x_文件);
-					} catch (Exception e) {
-						e.printStackTrace();
+						xtool.x_HTML_to_Byte_to_Response(x_info, response);
+					} catch (Exception e1) {
+						e1.printStackTrace();
 					}
-					// x_传输文件流(request, response, x_文件);
-					// x文件下载段_传输视频(request, response);
-				// 怎么串
+					
+					// 视频请求
+					// File x_文件 = new File("D:\\视频\\超兽武装\\超兽武装 - RevEvolution 35.mp4");
+					// try {
+						// dowloadS.x_传输流(request, response, x_文件);
+					// } catch (Exception e) {
+					// 	e.printStackTrace();
+					// }
 				break;
 				case "x2":
-					xtool.x404_回复(response);
-					//x_下载_传输文件流(request, response, x_文件);
-				break;
-				case "x4":
-					xtool.x404_回复(response);
-				break;
-				default:
 					xtool.x404_回复(response);
 				break;
 			}
@@ -79,5 +85,5 @@ public class index extends HttpServlet {
 	public index() {
 		super();
 	}
-		
+	
 }
