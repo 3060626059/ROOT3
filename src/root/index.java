eec4770273230063d954a161dc48b4a58329d27d
lenovo <lenovo@169.254.180.238>
2019-09-09 14:52:51 +0800

    20190909

--- src/root/index.java
+++ src/root/index.java
@@ -24,12 +24,6 @@ public class index extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	                throws ServletException, IOException {
-		doGet(request, response);
-		
-	}
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	                throws ServletException, IOException {
 		
